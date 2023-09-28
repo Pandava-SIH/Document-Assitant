349b63c96a1415361fec80510abadccd63b735d9
Satyam Gupta <satyamgupta3803@gmail.com>
2023-09-28 12:19:57 +0530

    added functionality of generating a samle document

--- main/helpers/gen_doc.py
+++ main/helpers/gen_doc.py
@@ -5,18 +5,18 @@ import re
 
 context = []
 
-def get_summary(data_chunks: list[str]) -> str:
-    """Generate initial summary """
-    message = [{"role": "user", "content": text} for text in data_chunks]
-    # message.append({"role":"user", "content":"summerize this document"})
-    # chat completion method
-    t = time.time()
-    summary =  g4f.ChatCompletion.create(
-        model= g4f.models.gpt_35_turbo,
-        provider= g4f.Provider.You, #change model if required
-        messages= message
-    )
-    return summary, time.time() - t
+# def get_summary(data_chunks: list[str]) -> str:
+#     """Generate initial summary """
+#     message = [{"role": "user", "content": text} for text in data_chunks]
+#     # message.append({"role":"user", "content":"summerize this document"})
+#     # chat completion method
+#     t = time.time()
+#     summary =  g4f.ChatCompletion.create(
+#         model= g4f.models.gpt_35_turbo,
+#         provider= g4f.Provider.You, #change model if required
+#         messages= message
+#     )
+#     return summary
 
 
 def chat_reponse(prompt: str):
@@ -46,11 +46,11 @@ def generate_document(prompt: str):
     print(res)
 
 if __name__ == "__main__":
-    print(get_summary(split_text_into_chunks("""Hello !!""")))
     x = input()
-    generate_document(x)
+    # y = get_summary(split_text_into_chunks(" Generate specific format of " + x))
+    y = "Generate sample document of " + x
+    generate_document(y)
 
     while True :
         query = input()
         chat_reponse(query)
-
